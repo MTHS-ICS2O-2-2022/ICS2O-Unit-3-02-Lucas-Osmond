974490acb0d2323093d25214baca04b59791d3c7
Lucas-Osmond <124389349+Lucas-Osmond@users.noreply.github.com>
2023-04-03 16:41:17 +0000

    Added previous code

--- js/script.js
+++ js/script.js
@@ -1,5 +1,22 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
-// This file contains the JS functions for index.html
+// Created by: Lucas Osmond
+// Created on: March 2023
+// This file contains the JS functions for index.html 
+
+'use strict'
+/**
+ * This function calculates area and perimeter of triangle.
+ */
+function calculate () {
+  // input
+  const abase = parseInt(document.getElementById('a-base-of-trapezoid').value)
+  const bbase = parseInt(document.getElementById('b-base-of-trapezoid').value)
+  const height = parseInt(document.getElementById('height-of-trapezoid').value)
+
+  // process
+  const area = [(abase + bbase) / 2] * height
+
+  // output
+  document.getElementById('area').innerHTML = 'Area is: ' + area + ' cm²'
+}
